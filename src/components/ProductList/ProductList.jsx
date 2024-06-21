3bb49bffe36e0b2a598a4d4ee1ffce638c0ea779
HidemeFlow <145919878+HidemeFlow@users.noreply.github.com>
2024-06-21 17:04:05 +0300

    Update

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://185.39.29.22:8000/web-data', {
+        fetch('http://85.119.146.179:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
