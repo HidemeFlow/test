24f894e3b5a6ad86fb5ed68a9fe7e0bcd2870c00
HidemeFlow <145919878+HidemeFlow@users.noreply.github.com>
2024-06-21 17:46:14 +0300

    Update Play.jsx

--- src/components/Play/Play.jsx
+++ src/components/Play/Play.jsx
@@ -51,27 +51,30 @@ const Play = () => {
     }
 
     return (
-        <div className={"form"}>
-            <h3>Введите ваши данные</h3>
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Страна'}
-                value={country}
-                onChange={onChangeCountry}
-            />
-            <input
-                className={'input'}
-                type="text"
-                placeholder={'Улица'}
-                value={street}
-                onChange={onChangeStreet}
-            />
-            <select value={subject} onChange={onChangeSubject} className={'select'}>
-                <option value={'physical'}>Физ. лицо</option>
-                <option value={'legal'}>Юр. лицо</option>
-            </select>
+        <div>
+            хуй
         </div>
+        // <div className={"form"}>
+        //     <h3>Введите ваши данные</h3>
+        //     <input
+        //         className={'input'}
+        //         type="text"
+        //         placeholder={'Страна'}
+        //         value={country}
+        //         onChange={onChangeCountry}
+        //     />
+        //     <input
+        //         className={'input'}
+        //         type="text"
+        //         placeholder={'Улица'}
+        //         value={street}
+        //         onChange={onChangeStreet}
+        //     />
+        //     <select value={subject} onChange={onChangeSubject} className={'select'}>
+        //         <option value={'physical'}>Физ. лицо</option>
+        //         <option value={'legal'}>Юр. лицо</option>
+        //     </select>
+        // </div>
     );
 };
 
