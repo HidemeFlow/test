f5033a6fb89b19fcf499a7c848b4c79a0cef30ea
HidemeFlow <145919878+HidemeFlow@users.noreply.github.com>
2024-06-22 20:47:06 +0300

    dse

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,10 +8,7 @@ const Header = () => {
 
     return (
         <div className={'header'}>
-            <Button onClick={onClose}>Close</Button>
-            <span className={'username'}>
-                {user?.username}
-            </span>
+            <span className='sp'>1.234.567.890$</span>
         </div>
     );
 };
